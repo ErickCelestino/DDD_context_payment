def2e640eace4e4b96be4f573702b714da47aa27
Erick Celestino <erickcelestimo@gmail.com>
2023-08-22 11:06:02 -0300

    reafactor: add notifications validate

--- DDD_context_payment.Domain/Handlers/SubscriptionHandler.cs
+++ DDD_context_payment.Domain/Handlers/SubscriptionHandler.cs
@@ -69,6 +69,9 @@ public class SubscriptionHandler :
 
         AddNotifications(name, document, email, address, student, payment);
 
+        if(!IsValid)
+            return new CommandResult(false, "Não foi possível realizar sua assinatura");
+
         _repository.CreateSubscription(student);
 
         _emailService.Send(student.Name.ToString(), student.Email.Address,"Bem vindo ao site de cursos", "Sua assinatura foi criada");
@@ -112,6 +115,9 @@ public class SubscriptionHandler :
         student.AddSubscription(subscription);
 
         AddNotifications(name, document, email, address, student, payment);
+
+        if(!IsValid)
+            return new CommandResult(false, "Não foi possível realizar sua assinatura");
 
         _repository.CreateSubscription(student);
 
@@ -158,6 +164,9 @@ public class SubscriptionHandler :
         student.AddSubscription(subscription);
 
         AddNotifications(name, document, email, address, student, payment);
+        
+        if(!IsValid)
+            return new CommandResult(false, "Não foi possível realizar sua assinatura");
 
         _repository.CreateSubscription(student);
 
